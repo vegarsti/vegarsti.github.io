ee2fe21391e72e9e11dbd5d006ef079ad00276e9
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2017-12-19 16:33:15 +0100

    Create _drafts folder if it does not exist

--- create_draft.sh
+++ create_draft.sh
@@ -8,14 +8,12 @@ create_draft_post() {
     local filename="$1"
     local title="$2"
     local draft_dir="_drafts"
-    echo "$draft_dir/$filename.md"
-
+    mkdir -p "$draft_dir"
+    touch "$draft_dir/$filename.md"
     cat <<EOT >> "$draft_dir/$filename.md"
 layout: post
 title: $title
 EOT
-
-    cat "$draft_dir/$filename.md"
 }
 
 if [ "$#" -ne 2 ]; then
