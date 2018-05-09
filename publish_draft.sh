24a97ea0472014e9a82882c8f38f27083a1c2d43
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-05-09 15:05:03 +0200

    Some style changes

--- publish_draft.sh
+++ publish_draft.sh
@@ -4,18 +4,18 @@ publish() {
     local DRAFT_DIR="_drafts"
     local POSTS_DIR="_posts"
     printf "Drafts in draft directory $DRAFT_DIR:\n"
-    local my_array=(`find "$DRAFT_DIR" -name "*.md"`)
+    local MY_ARRAY=(`find "$DRAFT_DIR" -name "*.md"`)
     local INDEX=0
     local HUMAN_READABLE_INDEX=1
-    for element in "${my_array[@]}"
+    for ELEMENT in "${MY_ARRAY[@]}"
     do
         printf "\t $HUMAN_READABLE_INDEX "
-        basename "$element" .md
+        basename "$ELEMENT" .md
         let INDEX="${INDEX}"+1
         let HUMAN_READABLE_INDEX="${HUMAN_READABLE_INDEX}"+1
     done
     read -p "Publish which draft? (0 to quit.) " CHOICE
-    CHOICE_IF_INTEGER="$(echo "$CHOICE" | grep -E ^\-?[0-9]+$)"
+    local CHOICE_IF_INTEGER="$(echo "$CHOICE" | grep -E ^\-?[0-9]+$)"
     if [[ -z "$CHOICE_IF_INTEGER" ]]; then
         echo "Not a valid choice!"
         exit 1
@@ -25,13 +25,18 @@ publish() {
             exit 1
         fi
     fi
-    let ACTUAL_CHOICE="${CHOICE}"-1
-    local FILENAME="${my_array[$ACTUAL_CHOICE]}"
+    local ACTUAL_CHOICE="${CHOICE}"-1
+    if [[ "$ACTUAL_CHOICE" -lt 0 || "$ACTUAL_CHOICE" -gt "$INDEX"-1 ]]; then
+        echo "Not a valid choice!"
+        exit 1
+    fi
+    local FILENAME="${MY_ARRAY[$ACTUAL_CHOICE]}"
     local BASENAME="$(basename $FILENAME)"
+    local BASENAME_WITHOUT_FILE="$(basename $FILENAME .md)"
     local DATE=`date +%Y-%m-%d`
     local FULL_FILENAME="$POSTS_DIR/$DATE-$BASENAME"
-    mv "$FILENAME" "$FULL_FILENAME"
+    #mv "$FILENAME" "$FULL_FILENAME"
+    echo "Publishing draft '$BASENAME_WITHOUT_FILE'!"
 }
-
 
 publish
