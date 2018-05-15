b5902a594b48688c7fb1246ca3e05ec86cc0d758
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-05-15 13:00:36 +0200

    Enforce Shellcheck completely

--- publish_draft.sh
+++ publish_draft.sh
@@ -4,8 +4,11 @@ publish() {
     local DRAFT_DIR="_drafts"
     local POSTS_DIR="_posts"
     printf "Drafts in draft directory %s:\\n" "$DRAFT_DIR"
-    local DRAFT_FILES
-    DRAFT_FILES=(`find "$DRAFT_DIR" -name "*.md"`)
+    local DRAFT_FILES=()
+    while IFS= read -d $'\0' -r file; do
+        DRAFT_FILES=("${DRAFT_FILES[@]}" "$file")
+    done < <(find "$DRAFT_DIR" -name "*.md" -print0)
+
     local INDEX=0
     local HUMAN_READABLE_INDEX=1
     for ELEMENT in "${DRAFT_FILES[@]}"; do
@@ -14,7 +17,8 @@ publish() {
         (( INDEX="${INDEX}"+1 ))
         (( HUMAN_READABLE_INDEX="${HUMAN_READABLE_INDEX}"+1 ))
     done
-    read -p "Publish which draft? (0 to quit.) " CHOICE
+    local CHOICE
+    read -r -p "Publish which draft? (0 to quit.) " CHOICE
     local CHOICE_IF_INTEGER
     CHOICE_IF_INTEGER="$(echo "$CHOICE" | grep -E '^\-?[0-9]+$')"
     if [[ -z "$CHOICE_IF_INTEGER" ]]; then
@@ -31,16 +35,15 @@ publish() {
         echo "Not a valid choice!"
         exit 1
     fi
-    local FILENAME="${MY_ARRAY[$ACTUAL_CHOICE]}"
+    local FILENAME="${DRAFT_FILES[$ACTUAL_CHOICE]}"
     local BASENAME
     local BASENAME_WITHOUT_FILE
-    BASENAME="$(basename $FILENAME)"
-    BASENAME_WITHOUT_FILE="$(basename $FILENAME .md)"
-    echo "$BASENAME_WITHOUT_FILE"
+    BASENAME="$(basename "$FILENAME")"
+    BASENAME_WITHOUT_FILE="$(basename "$FILENAME" .md)"
     local DATE
     DATE="$(date +%Y-%m-%d)"
     local FULL_FILENAME="$POSTS_DIR/$DATE-$BASENAME"
-    #mv "$FILENAME" "$FULL_FILENAME"
+    mv "$FILENAME" "$FULL_FILENAME"
     echo "Publishing draft '$BASENAME_WITHOUT_FILE'!"
 }
 
